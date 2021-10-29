2f57d85ba223f8eb03bbfaec4525633d34de5653
phdafoe <phdafoe@gmail.com>
2021-10-29 21:03:43 +0200

    [WIP] - Added new views -

--- 29102021/AppCiceFashion/AppCiceFashion/Components/CustomMainView.swift
+++ 29102021/AppCiceFashion/AppCiceFashion/Components/CustomMainView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct CustomMainView: View {
+    
+    @State private var dressSelected = "Dress"
+    
     var body: some View {
         VStack{
             HStack {
@@ -37,6 +40,64 @@ struct CustomMainView: View {
                     Text("Menu Item 3")
                 }
             }
+            
+            HStack{
+                ForEach(typesDresses, id: \.self) { item in
+                    HStack {
+                        Button(action: {
+                            self.dressSelected = item
+                        }) {
+                            Text(item)
+                                .font(.system(size: 8, weight: .semibold, design: .default))
+                                .padding()
+                        }
+                        .foregroundColor(self.dressSelected == item ? .white : .black)
+                        .background(self.dressSelected == item ? Color.black : Color.clear)
+                        .cornerRadius(10)
+                    }
+                }
+            }
+            
+            detailLazyGridScrollView
+        }
+    }
+    
+    var detailLazyGridScrollView: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            ForEach(customData) { item in
+                LazyVGrid(columns: Array(repeating: GridItem(), count: 2)) {
+                    ForEach(item.myRows) { itemRow in
+                        CardCell(model: itemRow)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct CardCell: View {
+    
+    var model: Row
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Image(model.image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+            HStack{
+                VStack(alignment: .leading, spacing: 10) {
+                    Text(model.name).font(.headline)
+                    Text(model.price).fontWeight(.black)
+                }
+                Spacer()
+                Button(action: {
+                }) {
+                    Image("option")
+                        .renderingMode(.original)
+                        .foregroundColor(.black)
+                }
+                .padding(.trailing, 10)
+            }
         }
     }
 }
@@ -46,3 +107,28 @@ struct CustomMainView_Previews: PreviewProvider {
         CustomMainView()
     }
 }
+
+
+// MARK: - MOdel Data
+struct CustomType: Identifiable {
+    var id: Int
+    var myRows: [Row]
+}
+
+struct Row: Identifiable {
+    var id: Int
+    var name: String
+    var image: String
+    var price: String
+}
+
+var customSize = ["S", "M", "L", "XL"]
+var typesDresses = ["Dress", "Pants", "Blazers", "Jackets"]
+var customData = [CustomType(id: 0, myRows: [Row(id: 0, name: "Fit and Flare", image: "p1", price: "€199"),
+                                             Row(id: 1, name: "Fit and Summer", image: "p21", price: "€299"),
+                                             Row(id: 2, name: "Empire and Flare", image: "p12", price: "€399"),
+                                             Row(id: 3, name: "Fit and Vibes", image: "p22", price: "€499"),
+                                             Row(id: 4, name: "Empire and Flare", image: "p1", price: "€599"),
+                                             Row(id: 5, name: "Fit and Empire", image: "p12", price: "€699")])]
+
+
